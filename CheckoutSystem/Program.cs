92013122013373ccfcefb375c64dfb8ceac39730
rizwen butt <rizwen_butt@hotmail.com>
2020-11-09 22:59:23 +0000

    with specflow tests

--- CheckoutSystem/Program.cs
+++ CheckoutSystem/Program.cs
@@ -8,7 +8,7 @@ namespace CheckoutSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is to call the class create order and calculate the order amount");
             CreateOrder order = new CreateOrder();
             order.Total(1, 1, 1);
         }
